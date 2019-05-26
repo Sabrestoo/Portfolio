7d5f00b7ca1d73bdff997242769f5a1f028ae88d
stuart Summers <summers2483@hotmail.com>
2019-05-26 22:39:57 +0200

    Design elements updated. Added landing text

--- main.js
+++ main.js
@@ -28,3 +28,28 @@ window.addEventListener("scroll", event => {
     }
   });
 });
+
+const menuBtn = document.querySelector(".menuButton");
+const navbar = document.querySelector(".nav-bar");
+const link = document.querySelectorAll(".link");
+
+
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    navbar.classList.add("show");
+  
+    link.forEach(item => item.classList.add("show"));
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    navbar.classList.remove("show");
+
+    link.forEach(item => item.classList.remove("show"));
+    showMenu = false;
+  }
+}
